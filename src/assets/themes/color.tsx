1e0955cf4f3e9243c884383f9d925bf2c225adfc
suprajasribalaji <suprajasri.balaji@gmail.com>
2024-07-26 19:30:16 +0530

    color code file generated

---
+++ src/assets/themes/color.tsx
@@ -0,0 +1,14 @@
+export const ColorBlack = {
+    black: '#000000',
+    charcoalBlack: '#1D1D1F',
+    semiTransparentBlack: '#00000080',
+}
+
+export const ColorWhite = {
+    white: '#FFFFFF',
+}
+
+export const ColorGray = {
+    ashGray: '#5D5D5B',
+    lightGray: '#F0F0F0',
+}
