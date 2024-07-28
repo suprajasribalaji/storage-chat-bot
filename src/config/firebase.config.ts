8b060d1f5797eed3b51c633622b2a638ee49f784
suprajasribalaji <suprajasri.balaji@gmail.com>
2024-07-28 21:44:56 +0530

    Initialization of the config is done

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+//SDK's installation
+import { initializeApp } from "firebase/app";
+
+// Firebase's Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdQB6tQrrp7dXD6x70FFOaCZ1xIFGv13U",
+  authDomain: "warehouse-47c9c.firebaseapp.com",
+  projectId: "warehouse-47c9c",
+  storageBucket: "warehouse-47c9c.appspot.com",
+  messagingSenderId: "589857382373",
+  appId: "1:589857382373:web:5692757010cff126f6d90c"
+};
+
+// Firebase's app initialization
+const app = initializeApp(firebaseConfig);
