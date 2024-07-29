4a1121ee12324803fed6bb10f5dee7dc068831f7
suprajasribalaji <suprajasri.balaji@gmail.com>
2024-07-29 14:11:53 +0530

    Protected route is setup using context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { ReactNode } from "react";
 import AccessPage from "./pages/AccessPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import { AuthProvider, useAuth } from "./components/context/Authentication";
+
+
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const { currentUser } = useAuth();
+  return currentUser ? <>{children}</> : <Navigate to="/" />;
+};
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AccessPage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/signup" element={<SignupPage />}/>
-      </Routes>
-    </BrowserRouter> 
-  )
+      <AuthProvider>
+        <Routes>
+          <Route path="/access" element={<AccessPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          {/* <Route path="/" element={<ProtectedRoute><WareHousePage /></ProtectedRoute>} /> */}
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5000,
-//   }
-// })
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5000,
+  }
+})
