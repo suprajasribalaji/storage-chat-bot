35a86c5f101544e295f9a61f9d931394794b58f6
suprajasribalaji <suprajasri.balaji@gmail.com>
2024-08-05 19:25:27 +0530

    The home page design is upgraded

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,62 @@
-import { Button, Input } from "antd";
-import { UserOutlined, CloudUploadOutlined, SendOutlined } from '@ant-design/icons';
+import { Button, Input, Menu, Dropdown } from "antd";
+import { UserOutlined, CloudUploadOutlined, SendOutlined, LogoutOutlined } from '@ant-design/icons';
 import styled from "styled-components";
-import { ColorBlue, ColorWhite } from "../assets/themes/color";
+import { ColorBlue } from "../assets/themes/color";
+import type { MenuProps } from 'antd';
+import ProfilePicture from "../assets/images/ProfilePicture.png";
+import { useNavigate } from "react-router-dom";
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const StyledMenu = styled(Menu)`
+  background-color: ${ColorBlue.paleBlue};
+  color: ${ColorBlue.deepOceanBlue};
+
+  .ant-menu-item:hover {
+    background-color: ${ColorBlue.deepOceanBlue};
+    color: ${ColorBlue.paleBlue};
+  }
+`;
 
 const HomePage = () => {
+    const navigate = useNavigate();
+
+    const handleLogoutMenu = (e: { key: string }) => {
+        if (e.key === '12') {
+          navigate('/login');
+        }
+    };
+
+    const items: MenuItem[] = [
+        {
+          key: '1',
+          label: 'Profile',
+          icon: <UserOutlined />,
+        },
+        {
+          key: '12',
+          label: 'Logout',
+          icon: <LogoutOutlined />,
+          onClick: handleLogoutMenu,
+        }
+      ];
+
+    const menu = (
+        <StyledMenu
+            mode="vertical"
+            items={items}
+        />
+    );
+
     return (
         <StyledHomePage>
             <HomePageNavBar>
                 <HomePageHeader>
                     Warehouse
                 </HomePageHeader>
-                <CircleButton>
-                    <UserIcon />
-                </CircleButton>
+                <Dropdown overlay={menu} trigger={['click']} placement="bottomRight">
+                    <CircleButton />
+                </Dropdown>
             </HomePageNavBar>
             <HomePageContent>
                 <ChatBoxInput>
@@ -26,123 +70,124 @@ const HomePage = () => {
             </HomePageContent>
         </StyledHomePage>
     );
-}
+};
 
 export default HomePage;
 
 const StyledHomePage = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    width: 100%;
-    height: 100vh;
-    background-color: ${ColorBlue.paleBlue};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  height: 100vh;
+  background-color: ${ColorBlue.paleBlue};
 `;
 
 const HomePageNavBar = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: ${ColorBlue.steelBlue};
-    padding: 0.5% 0.9%;
-    font-family: cursive;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${ColorBlue.steelBlue};
+  padding: 0.5% 0.9%;
+  font-family: cursive;
 `;
 
 const HomePageHeader = styled.span`
-    font-size: 130%; 
+  font-size: 130%;
 `;
 
 const CircleButton = styled(Button)`
-    width: 2.5%;
-    height: 5vh;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 0.5%;
-    background-color: ${ColorWhite.white};
-    color: ${ColorBlue.deepOceanBlue};
-    border: none;
-    
-    &&&:hover {
-        background-color: ${ColorBlue.deepOceanBlue};
-        .anticon {
-            color: ${ColorWhite.white};
-        }
-    }
-`;
+  width: 3%;
+  height: 6vh;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0.5%;
+  border: none;
+  background-image: url(${ProfilePicture});
+  background-size: cover;
 
-const UserIcon = styled(UserOutlined)`
-    font-size: 120%;
-    color: ${ColorBlue.deepOceanBlue};
+  &&&:hover, &&&:focus {
+    background-image: url(${ProfilePicture});
+    background-size: cover;
+  }
 `;
 
 const HomePageContent = styled.div`
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: center;
-    overflow: auto;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  overflow: auto;
 `;
 
 const ChatBoxInput = styled.div`
-    width: 50%;
-    display: flex;
-    justify-content: flex-end;
-    position: relative;
-    margin-bottom: 2%;
+  width: 50%;
+  display: flex;
+  justify-content: flex-end;
+  position: relative;
+  margin-bottom: 2%;
 `;
 
 const StyledChatInput = styled(Input.TextArea)`
-    width: 100%;
-    min-height: 7vh !important;
-    max-height: 20vh;
-    border: none;
-    border-radius: 40px;
-    padding-right: 4rem; 
-    z-index: 1;
-    padding-left: 1.5rem;
-    resize: none;
-    padding-top: 1.8%;
+  width: 100%;
+  min-height: 7vh !important;
+  max-height: 20vh;
+  border: none;
+  border-radius: 40px;
+  padding-right: 6rem; 
+  z-index: 1;
+  padding-left: 1.5rem;
+  resize: none;
+  padding-top: 1.8%;
 
-    &::placeholder {
-        color: ${ColorBlue.grayishBlue};
-        opacity: 1;
-        position: absolute;
-        top: 50%;
-        left: 1.5rem;
-        transform: translateY(-50%);
-        white-space: nowrap;
-    }
+  &::placeholder {
+    color: ${ColorBlue.grayishBlue};
+    opacity: 1;
+    position: absolute;
+    top: 50%;
+    left: 1.5rem;
+    transform: translateY(-50%);
+    white-space: nowrap;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  scrollbar-width: none;
+
+  -ms-overflow-style: none;
 `;
 
 const UploadButton = styled(Button)`
-    position: absolute;
-    right: 4.1rem;
-    bottom: 0.45rem;
-    background: none;
-    border: none;
-    z-index: 2;
-    padding: 0; 
+  position: absolute;
+  right: 4.1rem;
+  bottom: 0.45rem;
+  background: none;
+  border: none;
+  z-index: 2;
+  padding: 0; 
 `;
 
 const SendButton = styled(Button)`
-    position: absolute;
-    right: 1rem; 
-    bottom: 0.45rem;
-    border: none;
-    background: none;
-    z-index: 2;
-    padding: 0; 
+  position: absolute;
+  right: 1rem; 
+  bottom: 0.45rem;
+  border: none;
+  background: none;
+  z-index: 2;
+  padding: 0; 
 `;
 
 const UploadIcon = styled(CloudUploadOutlined)`
-    color: ${ColorBlue.deepOceanBlue};
-    font-size: 1.3rem !important;
+  color: ${ColorBlue.deepOceanBlue};
+  font-size: 1.3rem !important;
 `;
 
 const SendIcon = styled(SendOutlined)`
-    color: ${ColorBlue.deepOceanBlue};
-    font-size: 1.2rem !important;
+  color: ${ColorBlue.deepOceanBlue};
+  font-size: 1.2rem !important;
 `;
